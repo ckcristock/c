76738c1185a1f755d5aaa9d414bb28a261075cd9
NestorCorvus <nestor.lima@corvuslab.co>
2021-10-04 13:38:51 -0500

    se le agrego botón para poder anular o activar un tercero

--- src/app/pages/ajustes/informacion-base/terceros/terceros.component.ts
+++ src/app/pages/ajustes/informacion-base/terceros/terceros.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { TercerosService } from './terceros.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { SwalService } from '../services/swal.service';
 
 @Component({
   selector: 'app-terceros',
@@ -24,7 +25,8 @@ export class TercerosComponent implements OnInit {
   }
   constructor( 
                 private _tercerosService: TercerosService, private fb: FormBuilder,
-                public router: Router
+                public router: Router,
+                private _swal: SwalService
               ) { }
 
   ngOnInit(): void {
@@ -40,9 +42,35 @@ export class TercerosComponent implements OnInit {
     this.loading = true;
     this._tercerosService.getThirdParties(params).subscribe((r:any) => {
       this.thirdParties = r.data.data;
-      this.pagination.collectionSize = r.total;
+      this.pagination.collectionSize = r.data.total;
       this.loading = false;
     });
   }
+
+  changeState(third, state){
+    let data = {
+      id: third.id,
+      state
+    }
+    this._swal.show({
+      icon: 'question',
+      title: '¿Estas Seguro?',
+      text: (data.state == 'Inactivo' ? '¡El Tercero se Anulará!': '¡El Tercero se Activará!')
+    }).then((r) =>{
+      if (r.isConfirmed) {
+        this._tercerosService.changeState(data).subscribe((r:any) =>{
+          this.getThirdParties();
+        this._swal.show({
+            icon: 'success',
+            title: 'Proceso Satisfactio',
+            text: (data.state == 'Inactivo' ? 'El tercero ha sido Anulado.' : 'El tercero ha sido Activado.'),
+            showCancel: false
+        }); 
+        });
+      }
+    });
+  }
+
+
 
 }

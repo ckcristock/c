3e1fe28b97e9924e86f75bd42798ed2529cb1b90
NestorCorvus <nestor.lima@corvuslab.co>
2021-10-22 15:40:58 -0500

    se ha arreglado el pre-liquidar de los funcionarios

--- src/app/pages/ajustes/informacion-base/funcionarios/detalle-funcionario/detalle-funcionario.component.ts
+++ src/app/pages/ajustes/informacion-base/funcionarios/detalle-funcionario/detalle-funcionario.component.ts
@@ -58,21 +58,21 @@ export class DetalleFuncionarioComponent implements OnInit {
     let data = {
       status
     }
-    this.detalleService.liquidar(data, this.id).subscribe((r:any) => {
-      this._swal.show({
-        icon: 'question',
-        title: '¿Estas Seguro?',
-        text: 'Se dispone a liquidar el empleado'
-      }).then((result) => {
-        if (result.isConfirmed) {
+    this._swal.show({
+      icon: 'question',
+      title: '¿Estas Seguro?',
+      text: 'Se dispone a liquidar el empleado'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.detalleService.liquidar(data, this.id).subscribe((r:any) => {
           this._swal.show({
             icon: 'success',
             title: 'Proceso Satisfactorio',
             text: 'El Funcionario ha sido liquidado con éxito.',
             showCancel: false
           });
-        }
-      });
+        });
+      }
     });
   }
 

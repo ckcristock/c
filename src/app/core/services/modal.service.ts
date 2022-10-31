20ee2ef57536b62b7bd8e2c414b68b0bbed7b01f
CesarFlorezCorvusLab <cesar.florez@corvuslab.co>
2022-10-31 18:10:55 -0500

    A1-63 - En Proceso

--- src/app/core/services/modal.service.ts
+++ src/app/core/services/modal.service.ts
@@ -1,33 +1,12 @@
 import { Injectable } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
   constructor(private modalService: NgbModal) { }
 
-  open(content, size = 'md', onclose? : (motivo?:any)=>any) {
-    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: size, scrollable: true })
-    .result.then((motivo)=>onclose(motivo), (motivo)=>onclose(motivo));
-  }
-  close() {
-    this.modalService.dismissAll();
-  }
-  openSm(content, onclose? : (motivo?:any)=>any) {
-    this.open(content,'sm',onclose);
-  }
-  openLg(content, onclose? : (motivo?:any)=>any) {
-    this.open(content,'lg',onclose);
-  }
-  openXl(content, onclose? : (motivo?:any)=>any) {
-    this.open(content,'xl',onclose );
-  }
-  openScrollableContent(content, onclose? : (motivo?:any)=>any) {
-    this.open(content,'lg',onclose);
-  }
-
-  /* open(content, size = 'md') {
+  open(content, size = 'md') {
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: size, scrollable: true });
   }
   close() {
@@ -44,5 +23,5 @@ export class ModalService {
   }
   openScrollableContent(content) {
     this.modalService.open(content, { scrollable: true, size: 'lg' });
-  } */
+  }
 }

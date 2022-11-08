8737e6f676bb743993122172a19b9a0cfc3b08ef
Dulby Mendez <dulby.mendez@innovating.com.co>
2022-11-08 18:00:28 -0500

    MM-56-EnProceso

--- src/app/pages/ajustes/configuracion/nomina/components/incapacidades-config/incapacidades-config.component.ts
+++ src/app/pages/ajustes/configuracion/nomina/components/incapacidades-config/incapacidades-config.component.ts
@@ -2,10 +2,11 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { catchError, debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs/operators';
+import { catchError, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { SwalService } from 'src/app/pages/ajustes/informacion-base/services/swal.service';
 import { NominaConfigService } from '../../nomina-config.service';
 import { environment } from 'src/environments/environment';
+import { ajax, map } from 'jquery';
 
 @Component({
   selector: 'app-incapacidades-config',
@@ -33,6 +34,8 @@ export class IncapacidadesConfigComponent implements OnInit {
   };
   pageSize: any = 10;
   cuentas: any;
+  buscandoCuenta: boolean = false;
+  busquedaCuentaFallida: boolean = false;
 
   constructor(
     private _nominaService: NominaConfigService,
@@ -43,108 +46,23 @@ export class IncapacidadesConfigComponent implements OnInit {
 
   ngOnInit(): void {
   }
-/*
-  getBodegas(page = 1) {
 
-    this.loading = true;
-    this.bodegaService.getBodegas(params).subscribe((r */
+  formatter = (cuentas: { Nombre_Niif: string , Codigo_Niif: string }) => cuentas.Nombre_Niif;
 
-    setFiltros(paginacion:boolean) {
-
-      let params:any = {};
-
-      params.tam = this.pageSize;
-
-      if(paginacion === true){
-        params.pag = this.page;
-      }else{
-        this.page = 1; // Volver a la página 1 al filtrar
-        params.pag = this.page;
-      }
-
-      if (this.filtros.nombre.trim() != "") {
-        params.nombre = this.filtros.nombre;
-      }
-
-      if (this.filtros.categoria.trim() != "") {
-        params.categoria = this.filtros.categoria;
-      }
-
-      if (this.filtros.vida_util.trim() != "") {
-        params.vida_util = this.filtros.vida_util;
-      }
-
-      if (this.filtros.depreciacion.trim() != "") {
-        params.depreciacion = this.filtros.depreciacion;
-      }
-
-      let queryString = Object.keys(params).map(key => key + '=' + params[key]).join('&');
-      return queryString;
-    }
-
-    resetValues(){
-      this.filtros = {
-        nombre:'',
-        categoria:'',
-        vida_util:'',
-        depreciacion:''
-      };
-    }
-
-  consultaFiltrada(paginacion:boolean = false, page = 1) {
-
-    /* this.pagination.page = page;
-    let params = {
-      ...this.pagination, ...this.filtros
-    } */
-
-    let params = this.setFiltros(paginacion);
-    if(params === ''){
-      this.resetValues();
-      return;
-    }
-
-    this.loading = true;
-    this.http.get(`${environment.ruta}php/plancuentas/filtrar_cuentas.php?`+{params}).subscribe((data:any) => {
-      console.log(data)
-      /* if (data.codigo == 'success') {
-        this.TiposActivosFijos = data.query_result;
-        this.TotalItems = data.numReg;
-      }else{
-        this.TiposActivosFijos = [];
-      } */
-
-      this.loading = false;
-
-    });
-  }
-//{ params: { coincidencia: term, tipo: 'niif' }}
-  //formatter1 = (x: { Nombre_Cuenta: string }) => x.Nombre_Cuenta;
-
-  buscandoCuenta: boolean = false;
-  busquedaCuentaFallida: boolean = false;
-  formatter = (x: { Nombre_Niif: string , Codigo_Niif: string }) => x.Nombre_Niif;
   search_cuenta_niif = (text$: Observable<string>) =>
   text$.pipe(
     debounceTime(300),
     distinctUntilChanged(),
     tap(() => (this.buscandoCuenta = true)),
     switchMap(term =>
-      //this.http.get(`${environment.ruta}php/tipoactivo/get_lista_tipo_activo.php?`+{search: term}).pipe(
-      this.http.get(`${environment.ruta}php/plancuentas/filtrar_cuentas.php?`+{search: term}).pipe(
-      tap((res) => {
-        console.log(typeof(res))
-        if(res){
-          /* res.filter((ele)=>{
-            ele.Codigo_Niif == term
-          }) */
-          this.buscandoCuenta = false
-        } else {
-          this.cuentas = res
-        }
+      this.http.get(`${environment.ruta}php/plancuentas/filtrar_cuentas.php?`, { params: { coincidencia: term, tipo: 'codigo' }}).pipe(
+        tap((res : Array<{ Nombre_Niif: string , Codigo_Niif: string }>) => {
           console.log(res)
-          this.busquedaCuentaFallida = false
-        }),
+          if(res){
+            this.buscandoCuenta = false
+            this.busquedaCuentaFallida = false
+          }
+          }),
         catchError(() => {
           this.busquedaCuentaFallida = true;
           return of([]);
@@ -167,6 +85,24 @@ export class IncapacidadesConfigComponent implements OnInit {
         timer: 1000
       })
     })
+  }
+
+  setAccount=(cuentas, identifier)=>{
+    let data = {
+      id: cuentas.id,
+      manager: identifier
+    }
+    console.log(data)
+    /* this._responsableNService.createUpdatePayrollManager(data)
+      .subscribe((res:any)=>{
+
+        this._swal.show({
+          title: 'Responsable de Nómina',
+          icon: 'success',
+          text: res.data,
+          timer: 1000
+        })
+      }) */
   }
 
 }

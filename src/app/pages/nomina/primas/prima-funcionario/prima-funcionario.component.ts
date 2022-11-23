33f3198e88751cd7b0550e5a9b0a58adca090ed2
Dulby Mendez <dulby.mendez@innovating.com.co>
2022-11-23 17:16:41 -0500

    23-11

--- src/app/pages/nomina/primas/prima-funcionario/prima-funcionario.component.ts
+++ src/app/pages/nomina/primas/prima-funcionario/prima-funcionario.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatPaginatorIntl } from '@angular/material';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { PrimasService } from '../primas.service';
 
 @Component({
@@ -11,7 +13,6 @@ import { PrimasService } from '../primas.service';
 export class PrimaFuncionarioComponent implements OnInit {
   loading: boolean;
   empleados: any;
-  ulrTree: any;
   page: number;
   previousPage: number;
   paginacion = true;
@@ -21,14 +22,21 @@ export class PrimaFuncionarioComponent implements OnInit {
     collectionSize: 0,
   }
   totalPag: any;
+  periodoObs: Observable<number>;
+  anioObs: Observable<number>;
+  parametros: Observable<string>
+  anio: any;
+  periodo: any;
 
   constructor(
     private _primas: PrimasService,
-    private router: Router,
     private paginator: MatPaginatorIntl,
+    private route: ActivatedRoute
   ) {
-    this.ulrTree = this.router.parseUrl(this.router.url);
     this.paginator.itemsPerPageLabel = "Items por página:";
+
+    this.anioObs = route.params.pipe(map(p=> p.anio));
+    this.periodoObs = route.params.pipe(map(p=> p.periodo));
   }
 
   ngOnInit(): void {
@@ -39,15 +47,27 @@ export class PrimaFuncionarioComponent implements OnInit {
 
   calcularPrimas(){
     this.loading = true;
-    let params = {
-      fecha_inicio: new Date("07/01/2022"),
-      fecha_fin: new Date("12/31/2022")
+    this.anioObs.subscribe(params=>this.anio = params);
+    this.periodoObs.subscribe(params=>this.periodo = params);
+
+    let params: any;
+    if (this.periodo == 1) {
+      params = {
+        fecha_inicio: new Date(`01/01/${this.anio}`),
+        fecha_fin: new Date(`06/30/${this.anio}`)
+      }
+    } else {
+      params = {
+        fecha_inicio: new Date(`07/01/${this.anio}`),
+        fecha_fin: new Date(`12/31/${this.anio}`)
+      }
     }
+
     this._primas.setBonus(params)
     .subscribe((r:any)=>{
       this.empleados = r.data;
+      this.pagination.collectionSize = r.data.total_empleados;
       this.totalPag = r.data.total_empleados;
-      console.log(r.data.total_empleados);
       this.loading = false;
     });
   }
--- src/app/pages/nomina/primas/primas.component.ts
+++ src/app/pages/nomina/primas/primas.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { SwalService } from '../../ajustes/informacion-base/services/swal.service';
 import { PrimasService } from './primas.service';
 
 @Component({
@@ -18,16 +19,18 @@ export class PrimasComponent implements OnInit {
   years: any[] = [];
   premiums: any[] = [];
   premiumsPeople: any[] = [];
-
+  year = new Date().getFullYear();
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private modalService: NgbModal,
-    private _primas: PrimasService
+    private _primas: PrimasService,
+    private _swal: SwalService
   ) { }
 
   ngOnInit(): void {
+    console.log(this.year);
     this.createForm();
     // this.getPrimasList();
     let year = new Date().getFullYear();
@@ -44,8 +47,40 @@ export class PrimasComponent implements OnInit {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
-  private getDismissReason(reason: any) {
 
+  openConfirm2(){
+    let mes = new Date().getMonth();
+    let semestre = 1;
+    let lapso: string ;
+    if (mes<=6) {
+      semestre = 1;
+      lapso = ' enero - junio ';
+    } else {
+      semestre = 2;
+      lapso = ' julio - diciembre ';
+
+    }
+    let params = {
+      //periodo: this.form.get('periodo').value,
+      //yearSelected: this.form.get('year').value,
+      periodo: semestre,
+      yearSelected: this.year,
+      fecha_inicio: new Date("07/01/2022"),
+      fecha_fin: new Date("12/31/2022")
+    }
+    this._swal.show({
+      title: 'Prima',
+      text: `¿Desea pagar primas del ${semestre} semestre? (periodo: ${lapso})`,
+      icon: 'warning',
+      showCancel: true
+    }, ((res)=>{
+      if (res) {
+        this.router.navigate(['/nomina/prima', params.yearSelected, params.periodo])
+      }
+    }))
+  }
+
+  private getDismissReason(reason: any) {
   }
 
   openModal() {

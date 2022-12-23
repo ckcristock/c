02048a19cb97a8467fe3fcf997018266bfb3e0a8
CesarFlorezCorvusLab <cesar.florez@corvuslab.co>
2022-12-23 12:59:40 -0500

    Cierre 2022 - Layout inicial Catalogo

--- src/app/pages/ajustes/informacion-base/catalogo/catalogo.component.ts
+++ src/app/pages/ajustes/informacion-base/catalogo/catalogo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
+import { CategoriasService } from '../../parametros/categorias/categorias.service';
 
 @Component({
   selector: 'app-catalogo',
@@ -8,10 +9,53 @@ import { UserService } from 'src/app/core/services/user.service';
 })
 export class CatalogoComponent implements OnInit {
 
-  constructor( private _user: UserService ) { }
+  public Categorias: any[] = [];
+  public Subcategorias: any[] = [];
+  public selectedCategory: any = {
+    categoria: {
+      id: null,
+      nombre: null
+    },
+    subcategoria: {
+      id: null,
+      nombre: null
+    }
+  }
+  public pagination: any = {
+    page: 1,
+    pageSize: 5,
+    collectionSize: 0
+  }
+
+  constructor(
+    private _categoria: CategoriasService,
+    private _user: UserService
+  ) { }
 
   //ngbNavItem
   active = 1;
+  loadingCategorias: boolean = false;
+  loadingProductos: boolean = false;
+  Productos: any[] = [];
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getCategorias();
+  }
+
+  getCategorias() {
+    this.loadingCategorias = true;
+    this._categoria.getCategorias().subscribe((res: any) => {
+      this.Categorias = res.data;
+      this.loadingCategorias = false;
+    });
+  }
+
+  moveToTop() {
+    // window.scroll(0,0);
+    window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+     });
+ }
 }

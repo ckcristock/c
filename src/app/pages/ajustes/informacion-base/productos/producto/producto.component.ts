326bc10492b0f5322a01315c1aecd4da2cf8d239
CesarFlorezCorvusLab <cesar.florez@corvuslab.co>
2023-01-27 17:55:40 -0500

    MM-109 - Terminado

--- src/app/pages/ajustes/informacion-base/productos/producto/producto.component.ts
+++ src/app/pages/ajustes/informacion-base/productos/producto/producto.component.ts
@@ -12,31 +12,49 @@ import { CatalogoService } from '../../catalogo/catalogo.service';
 export class ProductoComponent implements OnInit {
   public productoPorVer: any = {};
   public Actividades:any[]=[];
-  public Mostrar=false;
+  public VariablesProducto:any={
+    categoria: {nom:[], valor:[]},
+    subcategoria: {nom:[], valor:[]}
+  };
   public cargando: any = {
-    actividad: false
+    actividad: false,
+    producto: false,
+    varsProducto: false,
+    valor: () => {
+      return this.cargando.actividad && this.cargando.producto && this.cargando.varsProducto;
+    }
   }
 
   constructor(
     private route: ActivatedRoute,
-    private http: HttpClient,
     private _catalogo: CatalogoService
   ){}
 
   ngOnInit() {
     let id = this.route.snapshot.params["id"];
     /* this.http.get(environment.ruta + 'php/productos/producto_ver.php', */
-    this.cargando.actividad = true;
+    this.cargando.producto = true;
     this._catalogo.getData({ id: id }).subscribe((res: any) => {
-      this.productoPorVer = res.data.data;
-      this.cargando.actividad = false;
+      this.productoPorVer = res.data;
+      this.cargando.producto = false;
     });
     /* this.http.get(environment.ruta+'php/productos/actividades.php',{ */
+    this.cargando.actividad = true;
     this._catalogo.getActividad({ id: id }).subscribe((res:any)=>{
       this.Actividades=res.data;
-      if(this.Actividades.length==0){
-        this.Mostrar=true;
-      }
+      this.cargando.actividad = false;
+    });
+    this.cargando.varsProducto = true;
+    this._catalogo.getCampos({ producto: id }).subscribe((res:any)=>{
+      res.data.cat.forEach(catVar => {
+        this.VariablesProducto.categoria.nom.push(catVar.label);
+        this.VariablesProducto.categoria.valor.push((catVar.valor)?catVar.valor:"<Por definir>");
+      });
+      res.data.subcat.forEach(subcatVar => {
+        this.VariablesProducto.subcategoria.nom.push(subcatVar.label);
+        this.VariablesProducto.subcategoria.valor.push((subcatVar.valor)?subcatVar.valor:"<Por definir>");
+      });
+      this.cargando.varsProducto = false;
     });
 
   }

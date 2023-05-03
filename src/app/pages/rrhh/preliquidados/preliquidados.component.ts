87cc0dae8d6fa391a4abe3dd6cf153e04a82a458
mariafdacevedo <128063287+mafecorvuslab@users.noreply.github.com>
2023-05-03 16:51:03 -0500

    M266 finalizado M273 terminado

--- src/app/pages/rrhh/preliquidados/preliquidados.component.ts
+++ src/app/pages/rrhh/preliquidados/preliquidados.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { PreliquidadosService } from './preliquidados.service';
 import { SwalService } from '../../ajustes/informacion-base/services/swal.service';
 import * as moment from 'moment';
-import { MatAccordion } from '@angular/material';
+import { MatAccordion, PageEvent } from '@angular/material';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { UserService } from 'src/app/core/services/user.service';
 import { DetalleService } from '../../ajustes/informacion-base/funcionarios/detalle-funcionario/detalle.service';
-import { debounceTime } from 'rxjs/operators';
+import { debounceTime, map, reduce, toArray } from 'rxjs/operators';
+import { PaginatorService } from 'src/app/core/services/paginator.service';
+import { Location } from '@angular/common';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-preliquidados',
@@ -21,22 +24,26 @@ export class PreliquidadosComponent implements OnInit {
   preliquidados: any = [];
   preliquidadosSelect: any = [];
   responsable: any = {};
-  formFilters: FormGroup;
-  loading: boolean = false;
   matPanel: boolean;
   people: any[] = [];
   diffDays: any;
+
+  loading: boolean = false;
+  formFilters: FormGroup;
+  active_filters: boolean = false
+  orderObj: any
+  paginationMaterial: any;
   pagination: any = {
     page: 1,
     pageSize: 10,
-    collectionSize: 0
+    //collectionSize: 0
   }
 
 
   listPreliquidados: any = []; //countries: [];
-  page = 1;
-  pageSize = 10;
-  collectionSize = 0;
+  //page = 1;
+  //pageSize = 10;
+  //collectionSize = 0;
 
 
   constructor(
@@ -46,15 +53,54 @@ export class PreliquidadosComponent implements OnInit {
     private fb: FormBuilder,
     private _user: UserService,
     private _detalle: DetalleService,
+    private route: ActivatedRoute,
+    private _paginator: PaginatorService,
+    private location: Location
   ) { }
 
   ngOnInit(): void {
     this.createForm();
-    this.getPreliquidados();
-    this.getPreliquidadosList();
-    this.responsable = this._user.user;
+    this.route.queryParamMap.subscribe((params: any) => {
+      if (params.params.pageSize) {
+        this.pagination.pageSize = params.params.pageSize
+      } else {
+        this.pagination.pageSize = 10
+      }
+      if (params.params.pag) {
+        this.pagination.page = params.params.pag
+      } else {
+        this.pagination.page = 1
+      }
+      this.orderObj = { ...params.keys, ...params }
+      if (Object.keys(this.orderObj).length > 3) {
+        this.active_filters = true
+        const formValues = {};
+        for (const param in params) {
+          formValues[param] = params[param];
+        }
+        this.formFilters.patchValue(formValues['params']);
+      }
+      this.getPreliquidados();
+      this.getPreliquidadosList();
+      this.responsable = this._user.user;
+    })
+      
   }
 
+  handlePageEvent(event: PageEvent) {
+    this._paginator.handlePageEvent(event, this.pagination)
+    this.getPreliquidados();
+  }
+
+  resetFiltros() { 
+    this._paginator.resetFiltros(this.formFilters)
+    this.active_filters = false
+  }
+
+  SetFiltros(paginacion) {
+    return this._paginator.SetFiltros(paginacion, this.pagination, this.formFilters)
+  }
+    //ok
   createForm() {
     this.formFilters = this.fb.group({
       person_id: ''
@@ -69,7 +115,7 @@ export class PreliquidadosComponent implements OnInit {
   openModal() {
     this.modal.show();
   }
-
+  //ok
   openClose() {
     this.matPanel = !this.matPanel;
     this.matPanel ? this.accordion.openAll() : this.accordion.closeAll();
@@ -77,7 +123,7 @@ export class PreliquidadosComponent implements OnInit {
 
   getPreliquidadosList() {
     this._preliquidadosService.getPreliquidados().subscribe((res: any) => {
-      this.preliquidadosSelect = res.data;
+      this.preliquidadosSelect = res.data.data;
       this.preliquidadosSelect.unshift({ full_names: 'Todos', id: '' });
     })
   }
@@ -85,16 +131,41 @@ export class PreliquidadosComponent implements OnInit {
   getPreliquidados() {
     this.loading = true;
     let params = {
+      ...this.pagination,
       ...this.formFilters.value
     }
+    var paramsurl = this.SetFiltros(this.pagination.page);
+    this.location.replaceState('rrhh/liquidados', paramsurl.toString());
     this._preliquidadosService.getPreliquidados(params)
       .subscribe((res: any) => {
-        this.preliquidados = res.data;
-        this.listPreliquidados = res.data;
-
-        this.collectionSize = res.data.length;
+        this.preliquidados = res.data.data;
+        this.listPreliquidados = res.data.data;
+        of(...this.listPreliquidados).pipe(
+          map(({ one_preliquidated_log, ...rest }) => ({
+            ...rest,
+            one_preliquidated_log: {
+              ...one_preliquidated_log,
+              created_at_timestamp: new Date(one_preliquidated_log.created_at).getTime()
+            }
+          })),
+          toArray(),
+          map(arr => arr.sort((a, b) => b.one_preliquidated_log.created_at_timestamp - a.one_preliquidated_log.created_at_timestamp)),
+          map(arr => arr.map(({ one_preliquidated_log: { created_at_timestamp, ...log }, ...rest }) => ({
+            ...rest,
+            one_preliquidated_log: {
+              ...log,
+              created_at: new Date(created_at_timestamp).toISOString()
+            }
+          }))),
+        ).subscribe(sortedData =>  this.listPreliquidados = sortedData);
+        
         this.loading = false;
-        this.refreshPreLiquidated()
+        this.paginationMaterial = res.data
+        if (this.paginationMaterial.last_page < this.pagination.page) {
+          this.paginationMaterial.current_page = 1
+          this.pagination.page = 1
+          this.getPreliquidados();
+        }
         /* for (let index = 0; index < this.preliquidados.length; index++) {
           let fecha = this.preliquidados[index].log_created_at;
           let InfoH = fecha;
@@ -103,12 +174,12 @@ export class PreliquidadosComponent implements OnInit {
       })
   }
 
-  refreshPreLiquidated() {
-    this.listPreliquidados = this.preliquidados.map((preliq, i) => ({ id: i + 1, ...preliq })).slice(
-      (this.page - 1) * this.pageSize,
-      (this.page - 1) * this.pageSize + this.pageSize,
-    );
-  }
+  // refreshPreLiquidated() {
+  //   this.listPreliquidados = this.preliquidados.map((preliq, i) => ({ id: i + 1, ...preliq })).slice(
+  //     (this.page - 1) * this.pageSize,
+  //     (this.page - 1) * this.pageSize + this.pageSize,
+  //   );
+  // }
 
   cantidadDate(fecha) {
     let now = moment(fecha).startOf('D').fromNow();
